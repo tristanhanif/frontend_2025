dc98eb8a5bf18c8a9a8bcf0d96125a45c0eed3d2
tristanhanif <tristanhanif1769@gmail.com>
2025-10-24 09:56:16 +0700

    hapus halaman page

--- app/guru/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function guruPage() {
-    return <>guru</>
-}
--- app/guru/ujian/[namaGuru]/tes/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function tesPage({ params }: { params: { namaGuru: string } }) {
-  return <>
-  <h1>Guru</h1>
-  <p>Nama Guru: {params.namaGuru}</p>
-  </>;
-}
